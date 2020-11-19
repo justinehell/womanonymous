379104985ff8821ca0b6f68b45372bff544d9f6f
alyson-b69 <alyson.bernabeu@gmail.com>
2020-11-19 16:25:27 +0100

    add cycle Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Cycle from "./components/Cycle";
 
 function App() {
   return (
     <div className="App">
       <h1>This is the App</h1>
+      <Cycle />
     </div>
   );
 }
