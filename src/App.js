de33acbe5ea446ee111ee41e97360f3314fccdbd
alyson-b69 <alyson.bernabeu@gmail.com>
2020-11-19 19:06:33 +0100

    import Fonts from google and add some style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cycle from "./components/Cycle";
 function App() {
   return (
     <div className="App">
-      <h1>This is the App</h1>
+      <h1>Baby Stop</h1>
       <Cycle />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./assets/reset.css";
 import App from "./App";
 
 ReactDOM.render(
