81a2126ff1337d3da95658785aedc8dc32a8f676
P-P-P-Pablo <pvilella.pro@gmail.com>
2020-11-19 18:55:57 +0100

    rotating line with the day

---
+++ src/components/InnerCycle.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+
+const InnerCycle = (props) => {
+    const [day, setDay] = useState(props.dayOfCycle);
+    
+	useEffect(() => {
+		setDay(props.dayOfCycle);
+	}, [props]);
+
+	return (
+		<div class='clock'>
+			<div
+				class='hand'
+				style={{ transform: `rotate(${day * (360 / 28)}deg)` }}></div>
+		</div>
+	);
+};
+export default InnerCycle;
