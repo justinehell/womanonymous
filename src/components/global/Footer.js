60aee32502ac7ca76fb6241f2e7b63693cebde49
Marine <marinemontailler@gmail.com>
2020-11-20 00:21:36 +0100

    remove img in footer

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { FaMask } from "react-icons/fa";
-import baroquefooter from '../../img/baroquefooter.png';
 
 
 function Footer() {
   return (
     <div className="footer">
-      <img src={baroquefooter} className="background-baroque" />
       <div className="footer-text">
         <p>
           <FaMask /> The Womanonymous to serve you{" "}
