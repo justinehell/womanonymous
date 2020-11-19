952d714936fb7e04c2ca89635e3269c8faac103e
P-P-P-Pablo <pvilella.pro@gmail.com>
2020-11-19 18:55:57 +0100

    rotating line with the day

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Cycle from "./components/Cycle";
+import InnerCycle from "./components/InnerCycle"
 
 function App() {
   return (
     <div className="App">
       <h1>This is the App</h1>
       <Cycle />
+      <InnerCycle dayOfCycle="8"/>
     </div>
   );
 }
