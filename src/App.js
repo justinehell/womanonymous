972e9bb4d5217aa1a234af05ca42743f531f4dcc
Marine <marinemontailler@gmail.com>
2020-11-19 21:23:00 +0100

    Auto stash before rebase of "origin/develop"

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import "./App.css";
 import Layout from "./Layout";
 import Cycle from "./components/Cycle";
 import Login from "./components/Login";
+import Burger from './components/Burger.js';
+import baroque from './img/baroque.png';
+import baroquefooter from './img/baroquefooter.png';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isHidden, setIsHidden] = useState(false)
+const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  function handleHidden() {
+    setIsHidden(!isHidden)
+  }
+  
   return (
     <div className="App">
       {isLoggedIn ? (
         <>
-          <h1>Baby Stop</h1>
-          <Layout />
+            <Burger handleHidden={handleHidden} isHidden={isHidden} />
+                <div id="overlay" className={isHidden ? 'overlay' : ''} onClick={isHidden ? handleHidden : ''}>
+                    <img src={baroque} className="background-baroque" />
+                    <h1>Baby Stop</h1>
+                    <Layout/>
+                    <img src={baroquefooter} className="background-baroque" />
+                </div>
         </>
       ) : (
         <Login setIsLoggedIn={setIsLoggedIn} />
