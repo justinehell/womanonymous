e3c18606ab2b2c8aff8c4d8383539b64f980c384
ShivanPaishari <shivan.paishari@yahoo.com>
2020-11-19 22:07:53 +0100

    blog without architecture

--- src/components/global/Main.js
+++ src/components/global/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
+import BlogListPage from "../specific/BlogListPage";
 
 
 function Main() {
   return (
     <div className="Main">
         <p>main</p>
+        <BlogListPage/>
     </div>
   );
 }
