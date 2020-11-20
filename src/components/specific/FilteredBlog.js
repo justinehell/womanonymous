6c1c1701da47ac6d47a2a224063484d89a77bd0c
ShivanPaishari <shivan.paishari@yahoo.com>
2020-11-20 12:55:47 +0100

    adding images in the blog

--- src/components/specific/FilteredBlog.js
+++ src/components/specific/FilteredBlog.js
@@ -3,12 +3,16 @@ import "./blogList.css";
 
 export default function FilteredBlog (props) {
     const title= props.articles.title;
-    const content= props.articles.content
+    const content= props.articles.content;
+    const imageArticles = props.articles.url; 
+    console.log(imageArticles);
    
 
     return (
         <div id="filtered-blog">
             <h3>{title}</h3>
+            {(imageArticles !== "" ? 
+            <div className="image-blog"><img  alt="blog" src={imageArticles}/></div> :"")}
             <p>{content}</p>
         </div>
     )
